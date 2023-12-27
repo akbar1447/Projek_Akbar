05fa805dd5daa226a93681b5938ee62de1f86f21
akbar <117895238+akbar1447@users.noreply.github.com>
2023-12-27 10:07:03 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('auth.Register');
 });
 
 Route::get('/barang', function () {
